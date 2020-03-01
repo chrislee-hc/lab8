c159fab3ce7d3f8159b5bf821c189c55a1ca9554
Christopher Lee <lee.christophersunjae@gmail.com>
2020-03-01 04:22:50 -0500

    Updates

--- lab8_part2.ml
+++ lab8_part2.ml
@@ -123,7 +123,7 @@ module MakeStack (Element: SERIALIZE) : (STACK with type element = Element.t) =
     let fold_left = List.fold_left
 
     let serialize (s : stack) : string =
-      if s = empty then raise Empty
+      if s = empty then ""
       else fold_left (fun x y -> Element.serialize y ^ ":" ^ x) (Element.serialize (top s)) (pop s)
 end ;;
 
